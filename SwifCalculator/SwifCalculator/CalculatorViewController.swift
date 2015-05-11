b11c2185ffcd12ead0bbefa5daefd0ffd88922ba
Adam Ahrens <adam.ahrens@smartthings.com>
2015-05-11 13:20:30 -0500

    Updating the layout constraints

--- SwifCalculator/SwifCalculator/CalculatorViewController.swift
+++ SwifCalculator/SwifCalculator/CalculatorViewController.swift
@@ -11,18 +11,25 @@ import UIKit
 class CalculatorViewController: UIViewController {
     @IBOutlet weak var calculatorDisplay: UILabel!
     
+    private var userIsTypingNumber = false
+    
     @IBAction func numberPressed(sender: UIButton) {
         let number = sender.currentTitle!
         if calculatorDisplay.text == "0" {
             calculatorDisplay.text = number
+            userIsTypingNumber = true
         } else {
             let updatedDisplay = calculatorDisplay.text! + number
             calculatorDisplay.text = updatedDisplay
         }
     }
     
+    @IBAction func operationPressed(sender: UIButton) {
+    }
+    
     @IBAction func clearPressed(sender: UIButton) {
         calculatorDisplay.text = "0"
+        userIsTypingNumber = false
     }
 }
 
